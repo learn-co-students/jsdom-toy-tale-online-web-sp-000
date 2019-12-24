f0a5d0abde5f659d1a821ad1af29db070db29100
chanwkkk <chanwk18@hotmail.com>
2019-12-24 14:05:31 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+
 let addToy = false
+let toys = document.getElementById("toy-collection")
+
+
 
 document.addEventListener("DOMContentLoaded", ()=>{
   const addBtn = document.querySelector('#new-toy-btn')
@@ -12,5 +16,50 @@ document.addEventListener("DOMContentLoaded", ()=>{
       toyForm.style.display = 'none'
     }
   })
-
+  
+  fetchToys()
 })
+
+
+function fetchToys(){
+  fetch("http://localhost:3000/toys")
+  .then(resp=>resp.json())
+  .then(json=>addJsonToBlock(json))
+}
+
+
+function addJsonToBlock(json){
+
+  for (const toy of json){
+    toys.innerHTML += 
+    `<div class='card'>
+    <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    <p>${toy.likes} Likes </p>
+    <button class="like-btn">Like</button>
+    </div>`
+  }
+}
+
+let form = document.querySelector("form")
+let inputs = document.querySelectorAll("input")
+
+
+form.addEventListener("submit", addNewToy)
+function addNewToy(){
+  let formData = {
+    name: inputs[0].value,
+    image: inputs[1].value
+  }
+  let obj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+  fetch("http://localhost:3000/toys",obj)
+  .then(resp=>resp.json())
+  .then(json=>alert(json))
+}
