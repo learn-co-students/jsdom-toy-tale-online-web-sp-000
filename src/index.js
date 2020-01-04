8b5cb6ac0e9b1258ce94190f53360e87529d66da
stabassum <sumu1993@gmail.com>
2020-01-04 17:29:10 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 // let addToy = false
 
-// document.addEventListener("DOMContentLoaded", ()=>{
-//   const addBtn = document.querySelector('#new-toy-btn')
-//   const toyForm = document.querySelector('.container')
-//   addBtn.addEventListener('click', () => {
-//     // hide & seek with the form
-//     addToy = !addToy
-//     if (addToy) {
-//       toyForm.style.display = 'block'
-//     } else {
-//       toyForm.style.display = 'none'
-//     }
-//   })
+document.addEventListener("DOMContentLoaded", ()=>{
+  const addBtn = document.querySelector('#new-toy-btn')
+  const toyForm = document.querySelector('.container')
+  addBtn.addEventListener('click', () => {
+    // hide & seek with the form
+    addToy = !addToy
+    if (addToy) {
+      toyForm.style.display = 'block'
+    } else {
+      toyForm.style.display = 'none'
+    }
+  })
 
-// })
+})
+
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 let addToy = false
