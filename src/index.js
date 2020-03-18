a62109efca3e5f897af48be5184d3f7386a0a9fa
BlythePollard <github email address>
2020-03-17 20:30:02 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 let addToy = false;
-var divCollect = document.querySelector('#toy-collection')
-
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyForm = document.querySelector(".container");
+  fetchToys()
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -16,22 +15,22 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+// const submitButton = document.querySelector('input[name="submit"]')
+// submitButton.addEventListener("click", postToy())
+
 function fetchToys() {
   fetch("http://localhost:3000/toys")
   .then(resp => resp.json())
   .then(json => {
-    console.log(json)
     json.forEach(toy => {
       addToys(toy)
     })
-   //with response data, make div class="card" for each toy & add it to collection div
-   //curly braces nullify implicit return of =>
   })
 }
 
 function addToys(toy) {
-  console.log(toy)
   const newDiv = document.createElement('div')
+  var divCollect = document.querySelector('#toy-collection')
   const h2 = document.createElement('h2')
     h2.innerHTML = toy.name
   let img = document.createElement('img')
@@ -39,18 +38,15 @@ function addToys(toy) {
   let p = document.createElement('p')
     p.innerHTML = toy.likes
   let button = document.createElement('button')
-  newDiv.class = "card"
-  button.class = "like-btn"
+  newDiv.classList.add = "card"
+  button.classList.add("like-btn")
   img.class = "toy-avatar"
   newDiv.append(h2, img, p, button)
-
-  //newDiv.innerHTML = toy.name
   divCollect.append(newDiv)
 }
 
-fetchToys()
-
-function postToy(toy) {
+function postToy(toy, event) {
+  event.preventDefault() //this makes it so that event can happen on submit WITHOUT page reloading
   fetch("http://localhost:3000/toys"), {
     method: "POST",
     headers: {
@@ -66,6 +62,27 @@ function postToy(toy) {
   .then(res => res.json())
   .then((toy_object) => {
     const newToy = addToys(toy_object)
-    divCollect.append(newToy)
+    divCollect.append
+  })
+  const submitButton = document.querySelector('input[name="submit"]')
+  submitButton.addEventListener("click", postToy())
+}
+
+function likes(toy, event) {
+  event.preventDefault()
+  likeCount = //likes object, incremented by 1
+  fetch(`http://localhost:3000/toys/${toy.target.id}`), {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "likes:" likeCount
+    })
+  }
+  .then(res => res.json())
+  .then(likedObject => {
+    ///likeCount?
   })
 }
