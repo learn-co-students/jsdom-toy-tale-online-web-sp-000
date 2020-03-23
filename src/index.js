eda0cf0ba9401ec34a1a2a8b73479f613b3195e1
Lauren McCoy <lm_lutec@outlook.com>
2020-03-22 21:05:24 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,64 @@ document.addEventListener("DOMContentLoaded", () => {
       toyForm.style.display = "none";
     }
   });
-});
+
+  fetch("http://localhost:3000/toys")   
+    .then(function(response){
+      return response.json();
+    })
+    .then(function(json){
+      console.log(json)
+      addToCollection(json) 
+      json.forEach(element => addToCollection(element));
+    
+
+    })
+
+
+  fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+      },
+  
+      body: JSON.stringify({
+            "name": "Jessie",
+            "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+            "likes": 0
+      })
+      // .then(function(response){
+      //   return response.json();
+      // })
+      // .then(function(json){
+      //   addToCollection(json) 
+      // })    
+    })
+
+
+function addToCollection(element){
+  let toyCollDiv = document.getElementById("toy-collection")
+
+  let createDiv = document.createElement("div")
+  let headerTwo = document.createElement("h2")
+  let toyImg = document.createElement("img")
+  let likes = document.createElement("p")
+  let likeButton = document.createElement("button")
+
+
+      headerTwo.innerHTML = element.name 
+      toyImg.src = element.image
+      toyImg.className = "toy-avatar"
+      likes.innerText = `${element.likes} likes`
+      likeButton.className = "like-btn"
+      likeButton.innerText = "Like <3"
+
+      createDiv.className = "card"
+      createDiv.appendChild(headerTwo)
+      createDiv.appendChild(toyImg)
+      createDiv.appendChild(likes)
+      createDiv.appendChild(likeButton)
+    
+      toyCollDiv.appendChild(createDiv)
+}
+})
