7f80f395370e6e0d79162beaa0b5e2b6caa068e5
github username <github email address>
2020-04-02 16:01:30 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,70 +1,96 @@
-
-let toyUrl = "http://localhost:3000/toys"
-let toyCollection = document.getElementById("toy-collection")
 let addToy = false;
 
-function getToys() {
-  return fetch (toyUrl)
+document.addEventListener('DOMContentLoaded', () => {
+  const addBtn = document.querySelector("#new-toy-btn");
+  const toyForm = document.querySelector(".container");
+  fetchToys()
+  addBtn.addEventListener("click", () => {
+    // hide & seek with the form
+    addToy = !addToy;
+    if (addToy) {
+      toyForm.style.display = "block";
+    } else {
+      toyForm.style.display = "none";
+    }
+  })
+})
+
+function fetchToys() {
+  fetch ("http://localhost:3000/toys")
   .then(resp => resp.json())
+  .then(json => {
+    json.forEach(toy => {
+      renderToys(toy)
+    })
+  })
 }
 
-function postToys(toy_data) {
-  return fetch (toyUrl, {
+function renderToys(toy) {
+    const card = document.createElement("div")
+    const toyCollection = document.getElementById("toy-collection")
+    
+    const h2 = document.createElement("h2")
+    h2.innerHTML = toy.name
+
+    const img = document.createElement("img")
+    img.setAttribute = ("src", toy.image)
+    img.setAttribute = ("class", "toy-avatar")
+
+    const p = document.createElement('p')
+    p.innerHTML = toy.likes
+
+    let button = document.createElement("button")
+    button.setAttribute = ("class", "like-btn")
+    button.innerText = "like"
+    button.addEventListener("click", event => {
+      likes(toy, event)
+    })
+
+    card.setAttribute = ("class", "card")
+    card.append(h2, img, p, button)
+    toyCollection.append(card)      
+ }
+
+
+// ------------------------------
+function postToys(toy, event) {
+  event.preventDefault()
+  return   fetch ("http://localhost:3000/toys"), {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "Accept": "application/json"
     },
     body: JSON.stringify({
-      "name": toy_data.name.value,
-      "image": toy_data.image.value,
+      "name": toy.name.value,
+      "image": toy.image.value,
       "likes": 0
     })
-  })
+  }
   .then(res => res.json())
   .then((toy_obj) => {
-    let new_toy = renderToys(toy_obj)
-    toyCollection.appendChild(new_toy)
+    const newToy = renderToys(toy_obj)
+    toyCollection.append
+  })
+  const submitButton = document.querySelector('input[name="submit"]')
+  submitButton.addEventListener("click", postToys())
+}
+
+function likes(toy, event) {
+  event.preventDefault()
+  const likeCount = parseInt(event.target.previousSibling.innerHTML, 10) + 1
+  fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      "likes": likeCount
+    })
+  })
+  .then(resp => resp.json())
+  .then(likedToy => {
+    event.target.previousSibling.innerHTML = likedToy.likes
   })
 }
-
-function renderToy(toy) {
-  let h2 = document.createElement("h2")
-  h2.innerHTML = toy.name
-
-  let img = document.createElement("img")
-  img.setAttribute = ("src", toy.image)
-  img.setAttribute = ("class", "toy-avatar")
-
-  let p = document.createElement('p')
-  p.innerHTML = toy.likes
-
-  let button = document.createElement("button")
-  button.setAttribute = ("class", "like-btn")
-
-  let card = document.createElement("div")
-  card.setAttribute = ("class", "card")
-  card.append(h2, img, p, button)
-  toyCollection.append(card)
-}
-
-  document.addEventListener("DOMContentLoaded", () => {
-    const addBtn = document.querySelector("#new-toy-btn");
-    const toyForm = document.querySelector(".container");
-    addBtn.addEventListener("click", () => {
-      // hide & seek with the form
-      addToy = !addToy;
-      if (addToy) {
-        toyForm.style.display = "block";
-        toyForm.addEventListener("submit", event => {
-          event.preventDefault() 
-          postToy(event.target)
-        })
-      } else {
-        toyForm.style.display = "none";
-      }
-    });
-  });
-
-
-getToys().then(toys => {toys.forEach(toy => {renderToys(toy)})})
