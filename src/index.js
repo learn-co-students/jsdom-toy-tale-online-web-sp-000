e6b6afbbed38bb4fadf90171fbaaeef26b754b49
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-05-30 11:54:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   const toyCollection = document.getElementById("toy-collection");
+  const toyForm = document.querySelector(".add-toy-form");
   
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
@@ -13,11 +14,13 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.addEventListener('submit', event => {
         event.preventDefault();
         postToy(event.target.name.value, event.target.image.value);
+        toyForm.reset();
       })
     } else {
       toyFormContainer.style.display = "none";
     }
   });
+ 
 
   function fetchToys() {
     return fetch('http://localhost:3000/toys')
@@ -47,8 +50,28 @@ document.addEventListener("DOMContentLoaded", () => {
       button.setAttribute("class", "like-btn");
       button.innerText = "Like <3";
 
+      button.addEventListener('click', event => {
+        toy.likes++
+        p.innerText = `${toy.likes} Likes `;
+        patchLikes(toy.likes, toy.id);
+      })
+
       div.append(h2, img, p, button);
     }
+  }
+  
+  function patchLikes(numLikes, id){
+    fetch(`http://localhost:3000/toys/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+
+      },
+      body: JSON.stringify({
+        likes: numLikes
+      })
+    })
   }
 
   function postToy(name, image) {
