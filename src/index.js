ded5e371728ad2d5f4bb27a075d636dd1c9b7fe6
The-Gentleman <zraja4402@gmail.com>
2020-11-17 12:14:29 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  fetchToys();
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -13,3 +14,40 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function fetchToys(){
+  fetch('http://localhost:3000/toys')
+    .then(response => response.json())
+    .then(json => getToys(json))
+}
+
+function getToys(json){ 
+  console.log(json)
+  let divCollection = document.getElementById("toy-collection");
+  for(let i = 0; i < json.length; i ++){
+    let div = document.createElement("div");
+    div.setAttribute('class', 'card');
+
+    let h2 = document.createElement("h2");
+    h2.innerHTML = json[i].name;
+
+    let image = document.createElement("img");
+    image.src = json[i].image;
+    image.setAttribute = ("class", "toy-avatar")
+    image.class = "toy-avatar"
+
+    let p = document.createElement("p");
+    p.innerHTML = `${json[i].likes} likes`;
+
+    let button = document.createElement("BUTTON");
+    button.innerHTML = "Like <3";
+    button.setAttribute("class", "like-btn")
+    
+    divCollection.appendChild(div);
+    div.appendChild(h2)
+    div.appendChild(image);
+    div.appendChild(p);
+    div.appendChild(button)
+  }
+}
+     
