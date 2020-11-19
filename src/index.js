d00cb8661a4069a74bea2c5255d2059f9d9e761a
chienleow <chienleow1@gmail.com>
2020-11-18 21:17:05 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  fetchToys();
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -13,3 +14,27 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function fetchToys (){
+  fetch ("http://localhost:3000/toys")
+  .then(response => response.json())
+  .then(toys => toys.forEach(toy => appendDiv(toy)));
+  // toys is the array of 8 toy objects
+}
+
+function appendDiv(toy){
+  const container = document.querySelector("#toy-collection");
+  const newDiv = document.createElement("div");
+  newDiv.classList.add('card');
+  container.appendChild(newDiv)
+  appendHeading(toy, container);
+}
+
+function appendHeading(toy, container){
+  const newHeading = document.createElement("h2");
+  newHeading.innerHTML = toy.name
+  container.appendChild(newHeading)
+}
+
+
+
