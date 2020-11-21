e490bb254982450be8f95e4042f63fd7d5a79135
github username <jaycru06@yahoo.com>
2020-11-21 08:05:12 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,28 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  // make a 'GET' request to fetch all the toy objects
+  let toyCollection = document.querySelector("#toy-collection");
+  let destUrl = "http://localhost:3000/toys";
+  let toyData = {
+    id: "",
+    name: "",
+    image: "",
+    likes: ""
+  };
+  let configObj = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(toyData)
+  }
+  fetch(destUrl, configObj)
+    .then(function(response) {
+      return JSON.response();
+    })
+    .then(function(toyObj) {
+      toyCollection.append(toyObj);
+    });
 });
