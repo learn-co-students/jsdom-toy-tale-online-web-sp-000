fa7ce3406265c486afae4471689b18a7cfe03ecc
Yumacorn <ReachRyanMui@gmail.com>
2020-11-29 10:29:40 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
 let addToy = false;
+const toyBox = document.getElementById("toy-collection");
+
+function collectToys() {
+  return fetch('http://localhost:3000/toys')
+    .then(function(response) {return response.json()})
+    .then(function(data) {
+      for (const checkToy in data) {
+        console.log(data[checkToy]);
+        console.log(data[checkToy].name);
+
+        let card = document.createElement("div");
+        card.setAttribute('class', 'card');
+        
+        let name = document.createElement("h2");
+        name.textContent = data[checkToy].name;
+        card.appendChild(name);
+        
+        let img = document.createElement("img");
+        img.setAttribute('class', 'toy-avatar')
+        img.src = data[checkToy].image;
+        card.appendChild(img);
+
+        let liked = document.createElement("p");
+        liked.textContent = `${data[checkToy].likes} likes`;
+        card.appendChild(liked);
+
+        toyBox.appendChild(card);
+      }
+      
+      console.log(`Filled the toyBox`)
+    });
+  }
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -12,4 +44,5 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  collectToys();
 });
