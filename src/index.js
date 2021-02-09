3ce14e2317a4db08f1d3e79373da14bc2d06ff69
jrog20 <jaimerogstad@gmail.com>
2021-02-08 19:14:46 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -12,4 +12,47 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  // My code starts here
+
+  fetch("http://localhost:3000/toys")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(object) {
+      for(const toy in object) {
+        makeToyCard(object[toy]);
+      }
+    });
+ 
+  function makeToyCard(toy) {
+    const toyCollection = document.querySelector("#toy-collection");
+    
+    let div = document.createElement("div");
+    div.classList.add("card");
+
+    let h2 = document.createElement("h2");
+    h2.textContent = toy.name;
+
+    let img = document.createElement("img");
+    img.classList.add("toy-avatar");
+    img.src = toy.image;
+
+    let p = document.createElement("p");
+    p.textContent = `${toy.likes} Likes`;
+
+    let likeBtn = document.createElement("button");
+    likeBtn.classList.add("like-btn");
+    likeBtn.textContent = "Like";
+    // button.addEventListener("click", addLike)
+
+    div.appendChild(h2);
+    div.appendChild(img);
+    div.appendChild(p);
+    div.appendChild(likeBtn);
+  }
+
+  // Add a New Toy
+
+  
+
 });
