93f60fe95d4b0d99337fdb24a2e63e26ddc52cbc
deliaconstantino <dconstan92@gmail.com>
2021-04-15 16:12:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-
   fetchAndAddToys();
 
   const addBtn = document.querySelector("#new-toy-btn");
@@ -17,115 +16,105 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-
 function fetchAndAddToys() {
   fetch("http://localhost:3000/toys")
-  .then(function(res) {
-    return res.json();
-  })
-  .then(function(json) {
-    json.forEach(element => createNewToyDiv(element));
-  })
-};
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (json) {
+      json.forEach((element) => createNewToyDiv(element));
+    });
+}
 
 function createNewToyDiv(element) {
   console.log(element);
   const toyCollection = document.querySelector("#toy-collection");
   const div = document.createElement("div");
   div.classList.add("card");
-  div.setAttribute('id', element.id);
+  div.setAttribute("id", element.id);
   addToyInfo(div, element);
   toyCollection.appendChild(div);
   // console.log(div);
-};
+}
 
 function addToyInfo(div, element) {
   const h2 = document.createElement("h2");
-  h2.innerHTML = element.name
+  h2.innerHTML = element.name;
   div.appendChild(h2);
 
   const img = document.createElement("IMG");
   img.classList.add("toy-avatar");
   img.src = element.image;
-  div.appendChild(img)
+  div.appendChild(img);
 
-  updateLikes(div, element)
+  const likesTag = displayLikes(div, element);
 
   const likeButton = document.createElement("button");
-  likeButton.onclick = function() {incrementLikes(element.likes, element.id)};
+  likeButton.onclick = function () {
+    incrementLikes(element, likesTag);
+  };
   likeButton.classList.add("like-btn");
   likeButton.innerHTML = "Like <3";
   div.appendChild(likeButton);
-};
+}
 
-  function updateLikes(div, element) {
-    const pTag = document.createElement("p");
-    pTag.innerHTML = `${element.likes} Likes`;
-    div.appendChild(pTag);
-  }
+function displayLikes(div, element) {
+  const pTag = document.createElement("p");
+  pTag.innerHTML = `${element.likes} Likes`;
+  div.appendChild(pTag);
+  return pTag;
+}
 
+const form = document.querySelector(".add-toy-form");
+
+form.addEventListener("submit", createNewToy);
+
+function createNewToy(event) {
+  event.preventDefault();
+  const toyName = form.querySelector("#add-toy-form-toy-name");
+  const imageUrl = form.querySelector("#add-toy-form-toy-image-url");
 
   const configObj = {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "Accept": "application/json"
+      Accept: "application/json",
     },
     body: JSON.stringify({
-      "name": "Jessie",
-      "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
-      "likes": 0
-    })
+      name: toyName.value,
+      image: imageUrl.value,
+      likes: 0,
+    }),
   };
 
   fetch("http://localhost:3000/toys", configObj)
-    .then(function(res) {
+    .then(function (res) {
       return res.json();
     })
-    .then(function(object) {
+    .then(function (object) {
       createNewToyDiv(object);
     });
+}
 
-    function incrementLikes(num, id) {
-      console.log(num);
-      console.log(num + 1);
-      const updateObj = {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
-        },
-        body: JSON.stringify({
-          numLikes: num,
-          id: id
-        })
-      }
+function incrementLikes(dbElement, likesTag) {
+  const updateObj = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      likes: dbElement.likes + 1,
+      id: dbElement.id,
+    }),
+  };
 
-      fetch(`http://localhost:3000/toys/${id}`, updateObj)
-      .then(function(res) {
-        return res.json();
-      })
-      .then(function(object) {
-        console.log(object);
-        // console.log(object.likes);
-        let div = document.getElementById(object.id);
-        updateLikes(div, object);
-        // let likes = document.querySelector("p");
-        // console.log(likes);
-        //
-        // likes.innerHTML = `${object.likes + 1} Likes`
-      });
-
-    };
-
-
-
-    // headers:
-    // {
-    //   "Content-Type": "application/json",
-    //   Accept: "application/json"
-    // }
-
-    // body: JSON.stringify({
-    //   "likes": <new number>
-    // })
+  fetch(`http://localhost:3000/toys/${dbElement.id}`, updateObj)
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (object) {
+      dbElement.likes = object.likes;
+      likesTag.innerHTML = `${object.likes} Likes`;
+    });
+}
